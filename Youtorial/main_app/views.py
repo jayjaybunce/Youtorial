de70b1987885b1849512479f32708fbc4ac540d8
Jayjaybunce <jayjaybunce>
2020-03-17 21:15:26 -0600

    Adds base and base_min

--- Youtorial/main_app/views.py
+++ Youtorial/main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'main_app/index.html')
+
+def user_profile(request):
+    return render(request, 'main_app/user_profile.html')
