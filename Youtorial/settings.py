15e12f526620a83a473162138c19ad6083d09339
Jayjaybunce <jayjaybunce>
2020-03-26 14:29:34 -0600

    Reverting

--- Youtorial/settings.py
+++ Youtorial/settings.py
@@ -80,10 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'youtorial',
-        'USER': 'postgres',
-        'PASSWORD': 'Cunch1p4123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+
 
     }
 }
